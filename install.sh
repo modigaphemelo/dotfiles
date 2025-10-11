94939927fddc986c03969d3a4128458cb776de6f
Modiga Phemelo <modigaphemelo96@gmail.com>
2025-10-12 01:38:51 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Arch Linux package installer script
+# This script installs the specified packages using pacman.
+# Run with sudo privileges or ensure you have sudo access.
+
+# List of packages to install
+PACKAGES="base-devel picom dunst git unzip zip ark 7zip ranger feh evince autotiling kitty code lxappearance libreoffice-still obsidian neovim polkit-gnome xdg-user-dirs ttf-liberation gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer-vaapi x265 x264 lame ffmpegthumbs mpv audacious audacious-plugins flameshot"
+
+echo "Starting installation of packages on Arch Linux..."
+echo "Packages to install: $PACKAGES"
+echo ""
+
+# Update package database
+sudo pacman -Sy --noconfirm
+
+# Install packages
+sudo pacman -S --needed $PACKAGES
+
+if [ $? -eq 0 ]; then
+    echo ""
+    echo "Installation completed successfully!"
+else
+    echo ""
+    echo "Installation encountered errors. Please check the output above."
+fi
